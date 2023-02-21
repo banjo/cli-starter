8e03b1112ce63aa2551bde7bc9a43c2328ee2279
Anton Ödman <anton.odman@gmail.com>
2023-02-21 21:20:33 +0100

    chore: first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# pkg-name
+
+[![NPM version](https://img.shields.io/npm/v/pkg-name?color=%23c53635&label=%20)](https://www.npmjs.com/package/pkg-name)
+
+A simple CLI application template.
+
+## Install
+
+```bash
+npm install pkg-name -g
+```
+
+## Usage
+
+```bash
+# run
+pkg-name
+
+# run subcommand
+pkg-name subcommand <name>
+```
---
+++ src/index.ts
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+import { cli, command as cliCommand } from "cleye";
+import { version } from "../package.json";
+
+console.log(); // Add a newline
+
+const name = "<name>";
+
+const subCommand = cliCommand(
+    {
+        name: "subcommand",
+        parameters: [name],
+        help: {
+            description: "Example subcommand",
+            usage: "subcommand <name>",
+        },
+    },
+    async (argv) => {
+        console.log(`Hello ${argv.name}`);
+    }
+);
+
+cli(
+    {
+        name: "pkg-name",
+        version,
+        commands: [subCommand],
+        help: {
+            description: "Example CLI",
+        },
+    },
+    async (argv) => argv.showHelp()
+);
